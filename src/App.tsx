c30765845230cd5af64b85622aee280c783957f2
Igor Josiel <igorjosielvitalino@gmail.com>
2022-08-07 10:58:37 -0300

    feat: acionando rota na primeira renderização da página.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import GlobalStyle from "./styles/globalStyles";
 import { Header } from "./components";
 import { ContainerScreen } from './App.styles';
@@ -6,11 +7,13 @@ import { RootState } from './App.types';
 import { searchNumberAction } from "./redux/fetchActions/searchNumberAction";
 
 function App() {
-  useSelector((store: RootState) => store?.searchNumber);
-
   const dispatch = useDispatch<any>();
 
-  dispatch(searchNumberAction());
+  useSelector((store: RootState) => store?.searchNumber);
+
+  useEffect(() => {
+    dispatch(searchNumberAction());
+  }, []);
 
   return (
     <>
